4f3b291db5215601b20d1081d33de363e9962707
Joshua.LeBlanc <jleblanc@omics.com>
2025-10-28 14:29:12 -0300

    add labels to graph

--- app/systems/graph_system.rb
+++ app/systems/graph_system.rb
@@ -8,6 +8,7 @@ class GraphSystem
             min_price = price_history.history.min
             price_range = max_price - min_price rescue -1
             
+            # Draw bars
             price_history.history.each_with_index do |price, index|
                 if price_range > 0
                     percentage = (price - min_price) / price_range
@@ -17,6 +18,49 @@ class GraphSystem
                 
                 h = percentage * rect.h
                 args.outputs.solids << { x: (rect.x + index * width) + 1, y: rect.y, w: width - 2, h: h }
+            end
+            
+            # Draw labels if we have price history
+            if price_history.history.length > 0
+                # Stock symbol at top
+                args.outputs.labels << {
+                    x: rect.x + rect.w / 2,
+                    y: rect.y + rect.h + 15,
+                    text: stock.symbol,
+                    size_px: 14,
+                    anchor_x: 0.5,
+                    anchor_y: 0.5
+                }
+                
+                # Max price label
+                args.outputs.labels << {
+                    x: rect.x - 5,
+                    y: rect.y + rect.h,
+                    text: max_price&.round(2).to_s,
+                    size_px: 10,
+                    anchor_x: 1,
+                    anchor_y: 0.5
+                }
+                
+                # Min price label
+                args.outputs.labels << {
+                    x: rect.x - 5,
+                    y: rect.y,
+                    text: min_price&.round(2).to_s,
+                    size_px: 10,
+                    anchor_x: 1,
+                    anchor_y: 0.5
+                }
+                
+                # Current price label
+                args.outputs.labels << {
+                    x: rect.x + rect.w + 5,
+                    y: rect.y + rect.h / 2,
+                    text: stock.current_price&.round(2).to_s,
+                    size_px: 12,
+                    anchor_x: 0,
+                    anchor_y: 0.5
+                }
             end
         end
     end
