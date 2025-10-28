28c466cff3febeae9fd2f786d51159159d21d90b
Joshua.LeBlanc <jleblanc@omics.com>
2025-10-28 14:25:08 -0300

    scale graph correctly

--- app/systems/graph_system.rb
+++ app/systems/graph_system.rb
@@ -6,9 +6,15 @@ class GraphSystem
             
             max_price = price_history.history.max
             min_price = price_history.history.min
+            price_range = max_price - min_price rescue -1
             
             price_history.history.each_with_index do |price, index|
-                percentage = price / max_price
+                if price_range > 0
+                    percentage = (price - min_price) / price_range
+                else
+                    percentage = 0.5
+                end
+                
                 h = percentage * rect.h
                 args.outputs.solids << { x: (rect.x + index * width) + 1, y: rect.y, w: width - 2, h: h }
             end
